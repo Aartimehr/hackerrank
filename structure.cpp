53a060c3f4702c4007442238cef500492b0d58d7
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-26 16:47:37 +0530

    Create structure.cpp

---
+++ structure.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+     struct details
+     {
+         int age;
+         char name [100];
+         char lname [100];
+         int standard;
+     };
+     int main()
+     {
+         details d;
+         cin>>d.age;
+         cin>>d.name;
+         cin>>d.lname;
+         cin>>d.standard;
+         
+         cout<<d.age<<" ";
+         cout<<d.name<<" ";
+         cout<<d.lname<<" ";
+         cout<<d.standard<<" ";
+         return 0;
+         
+     }
+        
+    
